13a8ad073def2ab716939ff5c6bc6d448cda06c1
Ayush Verma <ayve012@gmail.com>
2026-08-19 23:46:26 +0530

    frentend educator section in process

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavBar from "../../components/educator/NevBar";
+import SideBar from "../../components/educator/SideBar";
+import Footer from "../../components/educator/Footer";
 
 const Educator = () => {
   return (
-    <div>
-      <h1>Educator</h1>
-      <div>{<Outlet />}</div>
+    <div className="text-default min-h-screen bg-white">
+      <NavBar />
+      <div className="flex">
+        <SideBar />
+        <div className="flex-1">{<Outlet />}</div>
+      </div>
+      <Footer />
     </div>
   );
 };
